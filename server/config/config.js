dab0e6a64955e77f336643d398b3ff6efcc0832d
Elliott Jones <EllyottJones@hotmail.com>
2017-12-14 05:56:51 -0500

    Add GET /users/me and authentication middleware

--- server/config/config.js
+++ server/config/config.js
@@ -3,12 +3,13 @@ var env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test' || env === 'TEST') {
+} else { 
+	env = 'test';	
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} 
-// else { 
-// 	// console.log('Error: ', env);
+}
+
+// else if (env === 'test' || env === 'TEST') {
 // 	process.env.PORT = 3000;
 // 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
+// } 
